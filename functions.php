ded17104889088dbcfa7299704163d49c54c200e
Micael Macedo <micaelm2009@hotmail.com>
2023-05-24 23:13:36 -0300

    feat: add project

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+add_action('after_setup_theme', 'my_theme_setup');
+function my_theme_setup(){
+    add_theme_support( 'post-thumbnails');
+
+    add_theme_support( 'title-tag');
+
+    register_nav_menus( array('priamry' => 'Primary') );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_scripts');
+function my_theme_scripts(){
+    wp_enqueue_style( 'my-theme-bootsrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <h1><?php bloginfo('name'); ?></h1>
+        <h2><?php bloginfo('description'); ?></h2>
+        <?php
+        if(have_posts()) :
+            while (have_posts()) :
+                the_post();
+                ?>
+                <h3><?php the_title(); ?></h3>
+                
+                <div><?php the_content(); ?></div>
+                <?php
+            endwhile;
+        else:
+        ?>
+        <h3>404 - Not Found</h3>
+        <?php 
+            endif;
+        ?>
+        <?php wp_footer(); ?>
+    </body>
+</html>
